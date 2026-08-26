d9cc22680f134db1836dee323094e60e32788246
trung <qtrungyb@gmail.com>
2026-08-26 07:39:06 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4581,9 +4581,9 @@ function openBudgetManager() {
                     <div style="font-weight: 700; font-size: 14px; color: var(--text-main); white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">${c.name}</div>
                 </div>
                 
-                <div style="display: flex; gap: 6px; align-items: center; flex-shrink: 0; width: 190px;">
+                <div style="display: flex; gap: 6px; align-items: center; flex-shrink: 0; width: 145px;">
                     <div class="amount-wrapper" style="height: 36px; flex: 1; background: #f8f9fa; border: 1px solid transparent; transition: 0.3s; border-radius: 8px; margin-bottom: 0;">
-                        <input type="text" class="form-control input-amount budget-input-display" data-id="${c.id}" placeholder="Vô hạn" value="${budgetVal}" style="font-size: 14px; background: transparent; border: none; text-align: right; font-weight: 700; width: 100%; height: 100%; padding: 0 24px 0 8px !important;">
+                        <input type="text" class="form-control budget-input-display" data-id="${c.id}" placeholder="Vô hạn" value="${budgetVal}" style="font-size: 14px; background: transparent; border: none; text-align: right; font-weight: 700; width: 100%; height: 100%; padding: 0 24px 0 8px;">
 
                         <span class="amount-currency" style="right: 8px; top: 9px; font-size: 12px;">đ</span>
                         <input type="hidden" class="budget-input-raw" id="raw_budget_${c.id}" value="${rawVal}">
